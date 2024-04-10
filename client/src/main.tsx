4c7d883f71b558ca2a82b1bc9cff36c1443272cb
aplicaciones-servired <aplicaciones@gruposervired.com.co>
2024-04-10 16:39:19 -0500

    rutas

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,10 @@
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 
 const root = document.getElementById('root')
 
 if (root !== null) {
-  createRoot(root).render(<App />)
+  ReactDOM.createRoot(root).render(<BrowserRouter> <App /> </BrowserRouter>)
 }
