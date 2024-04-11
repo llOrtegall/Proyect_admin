f711248aa2ebee94328e02858add9ee0512883ee
llOrtegall <IvanOrtega_97@hotmail.com>
2024-04-10 20:39:00 -0500

    agregando home page a app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from 'react-router-dom'
+import Home from './pages/HomePage'
 
 function App (): JSX.Element {
   return (
     <>
       <Routes>
         <Route path="/login" element={<h1>Login</h1>} />
-        <Route path="/" element={<h1>Home Navbar</h1>} />
+        <Route path="/" element={ <Home /> } />
       </Routes>
     </>
   )
