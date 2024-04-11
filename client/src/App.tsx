f1a4ff76e66cab2944d1e8ba85569d0718370708
llOrtegall <IvanOrtega_97@hotmail.com>
2024-04-11 09:13:31 -0500

    agregando mas rutas de example

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,6 +9,9 @@ function App (): JSX.Element {
       <Routes>
         <Route path="/login" element={<h1>Login</h1>} />
         <Route path="/" element={ <Home /> } />
+        <Route path='/example1' element={<h1>Example 1</h1>} />
+        <Route path='/example2' element={<h1>Example 2</h1>} />
+        <Route path='/example3' element={<h1>Example 3</h1>} />
       </Routes>
     </>
   )
