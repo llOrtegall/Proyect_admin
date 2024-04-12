b7629c2a83129cf147e2da395650cf88d0f7d515
aplicaciones-servired <aplicaciones@gruposervired.com.co>
2024-04-12 12:51:43 -0500

    manejo errores

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,13 +1,15 @@
 import React, { useState } from 'react'
 import { Input, Button } from '../components/iu'
-import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 import { useAuth } from '../auth/AuthProvider'
+import axios from 'axios'
+import { WarningIcon } from '../components/iu/warning'
 
 function LoginPage (): JSX.Element {
   const { login } = useAuth()
   const [user, setUser] = useState('')
   const [password, setPassword] = useState('')
+  const [errorString, setErrorString] = useState('')
   const navigate = useNavigate()
 
   const handleSubmit = (ev: React.FormEvent): void => {
@@ -20,31 +22,49 @@ function LoginPage (): JSX.Element {
           navigate('/home')
         }
       })
-      .catch(err => { console.error(err) })
+      .catch(error => {
+        const errorString: string = error.response.data.message
+        setErrorString(errorString)
+        setTimeout(() => {
+          setErrorString('')
+        }, 5000)
+      })
   }
 
   return (
-        <section className="h-[100vh] flex flex-col items-center justify-center bg-gradient-to-b from-blue-700 to-blue-200">
-            <form className='w-96 mb-2 border p-12 rounded-lg bg-white/30 flex flex-col gap-4 shadow-xl' onSubmit={(ev) => { handleSubmit(ev) }}>
+    <section className="h-[100vh] flex flex-col items-center justify-center bg-gradient-to-b from-blue-700 to-blue-200">
+      <form className='w-96 mb-2 border p-12 rounded-lg bg-white/30 flex flex-col gap-4 shadow-xl' onSubmit={(ev) => { handleSubmit(ev) }}>
 
-                <article className='w-full flex flex-col gap-2'>
-                    <label className="flex justify-center uppercase">Usuario </label>
-                    <div className='flex justify-center mb-2'>
-                        <Input name='username' type='text' placeholder='CP1118342523' autoComplete='username' value={user}
-                            onChange={(ev) => { setUser(ev.target.value) }} />
-                    </div>
-                </article>
+        <article className='w-full flex flex-col gap-2'>
+          <label className="flex justify-center uppercase">Usuario </label>
+          <div className='flex justify-center mb-2'>
+            <Input name='username' type='text' placeholder='CP1118342523' autoComplete='username' value={user}
+              onChange={(ev) => { setUser(ev.target.value) }} />
+          </div>
+        </article>
 
-                <article className='w-full flex flex-col gap-2'>
-                    <label className="flex justify-center uppercase ">contraseña</label>
-                    <div className='flex justify-center mb-2'>
-                        <Input name='contraseña' type='text' placeholder='***********' autoComplete='contraseña' value={password}
-                            onChange={(ev) => { setPassword(ev.target.value) }} />
-                    </div>
-                </article>
-                <Button>Iniciar Sesión</Button>
-            </form >
-        </section >
+        <article className='w-full flex flex-col gap-2'>
+          <label className="flex justify-center uppercase ">contraseña</label>
+          <div className='flex justify-center mb-2'>
+            <Input name='contraseña' type='text' placeholder='***********' autoComplete='contraseña' value={password}
+              onChange={(ev) => { setPassword(ev.target.value) }} />
+          </div>
+        </article>
+        <Button>Iniciar Sesión</Button>
+      </form >
+
+      {(errorString !== '') &&
+        <div className='flex gap-2'>
+          <p className="text-center bg-red-600 text-white font-semibold p-2 rounded-md">
+            <WarningIcon />
+          </p>
+          <p className="text-center bg-red-600 text-white font-semibold p-2 rounded-md">
+            {errorString}
+          </p>
+        </div>
+      }
+
+    </section >
   )
 }
 
