9fa8b7f61a08e190929b595fcf89c6be857a8755
aplicaciones-servired <aplicaciones@gruposervired.com.co>
2024-04-12 09:09:04 -0500

    login.2

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -22,26 +22,27 @@ function LoginPage (): JSX.Element {
   }
 
   return (
-    <section className="h-[100vh] flex flex-col items-center justify-center bg-gradient-to-b from-blue-700 to-blue-200">
-      <form className='w-96 mb-2 border p-12 rounded-lg bg-white/30 flex flex-col gap-4 shadow-xl'>
+        <section className="h-[100vh] flex flex-col items-center justify-center bg-gradient-to-b from-blue-700 to-blue-200">
+            <form className='w-96 mb-2 border p-12 rounded-lg bg-white/30 flex flex-col gap-4 shadow-xl' >
 
-        <article className='w-full flex flex-col gap-2'>
-          <label className="flex justify-center uppercase">Usuario </label>
-          <div className='flex justify-center mb-2'>
-            <Input name='username' type='text' placeholder='CP1118342523' autoComplete='username' />
-          </div>
-        </article>
+                <article className='w-full flex flex-col gap-2'>
+                    <label className="flex justify-center uppercase">Usuario </label>
+                    <div className='flex justify-center mb-2'>
+                        <Input name='username' type='text' placeholder='CP1118342523' autoComplete='username' value={user}
+                            onChange={(ev) => { setUser(ev.target.value) }} />
+                    </div>
+                </article>
 
-        <article className='w-full flex flex-col gap-2'>
-          <label className="flex justify-center uppercase ">contraseña</label>
-          <div className='flex justify-center mb-2'>
-            <Input name='contraseña' type='text' placeholder='***********' autoComplete='contraseña' />
-          </div>
-        </article>
-
-        <Button >Iniciar Sesión</Button>
-      </form>
-    </section>
+                <article className='w-full flex flex-col gap-2'>
+                    <label className="flex justify-center uppercase ">contraseña</label>
+                    <div className='flex justify-center mb-2'>
+                        <Input name='contraseña' type='text' placeholder='***********' autoComplete='contraseña' value={password}
+                            onChange={(ev) => { setPassword(ev.target.value) }} />
+                    </div>
+                </article>
+                <Button onClick={handleSubmit}>Iniciar Sesión</Button>
+            </form>
+        </section>
   )
 }
 
