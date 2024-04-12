7262b7c06c3948e4ebd1561fb5679bbf8ece606c
aplicaciones-servired <aplicaciones@gruposervired.com.co>
2024-04-12 10:58:04 -0500

    login logica

--- client/src/components/iu/Button.tsx
+++ client/src/components/iu/Button.tsx
@@ -1,11 +1,10 @@
 interface ButtonProps {
   children: React.ReactNode
-  onClick?: () => void
 }
 
-export function Button ({ children, onClick }: ButtonProps): JSX.Element {
+export function Button ({ children }: ButtonProps): JSX.Element {
   return (
-      <button type="submit" className="p-2 bg-blue-500 rounded-lg text-white font-semibold hover:bg-blue-700" onClick={onClick}>
+      <button type="submit" className="p-2 bg-blue-500 rounded-lg text-white font-semibold hover:bg-blue-700">
       {children}
     </button>
   )
--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { Input, Button } from '../components/iu'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
@@ -6,24 +6,26 @@ import { useAuth } from '../auth/AuthProvider'
 
 function LoginPage (): JSX.Element {
   const { login } = useAuth()
-  const [user, setUser] = useState(' ')
-  const [password, setPassword] = useState(' ')
+  const [user, setUser] = useState('')
+  const [password, setPassword] = useState('')
   const navigate = useNavigate()
 
-  async function handleSubmit (): Promise<void> {
-    try {
-      const response = await axios.post('http://172.20.1.216:4040/login', { user, password })
-      login()
-      navigate('/home')
-      console.log('Login response:', response.data)
-    } catch (error) {
-      console.error('Error al iniciar sesión:', error)
-    }
+  const handleSubmit = (ev: React.FormEvent): void => {
+    ev.preventDefault()
+
+    void axios.post('http://172.20.1.216:4000/api/login', { user, password })
+      .then(res => {
+        if (res.data?.auth === true) {
+          login()
+          navigate('/home')
+        }
+      })
+      .catch(err => { console.error(err) })
   }
 
   return (
         <section className="h-[100vh] flex flex-col items-center justify-center bg-gradient-to-b from-blue-700 to-blue-200">
-            <form className='w-96 mb-2 border p-12 rounded-lg bg-white/30 flex flex-col gap-4 shadow-xl' >
+            <form className='w-96 mb-2 border p-12 rounded-lg bg-white/30 flex flex-col gap-4 shadow-xl' onSubmit={(ev) => { handleSubmit(ev) }}>
 
                 <article className='w-full flex flex-col gap-2'>
                     <label className="flex justify-center uppercase">Usuario </label>
@@ -40,9 +42,9 @@ function LoginPage (): JSX.Element {
                             onChange={(ev) => { setPassword(ev.target.value) }} />
                     </div>
                 </article>
-                <Button onClick={handleSubmit}>Iniciar Sesión</Button>
-            </form>
-        </section>
+                <Button>Iniciar Sesión</Button>
+            </form >
+        </section >
   )
 }
 
