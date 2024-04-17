5bbeedea6593c036798edccc8ccf2377a7e86a68
llOrtegall <IvanOrtega_97@hotmail.com>
2024-04-17 12:00:20 -0500

    changes to NabVar component

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -4,6 +4,20 @@ import { NavLink } from 'react-router-dom'
 import { Switch } from '@tremor/react'
 import { Button } from './iu'
 
+const Links = [
+  { link: '/home', name: 'Home' },
+  { link: '/example2', name: 'Example 2' },
+  { link: '/example3', name: 'Example 3' }
+]
+
+const LinkComponent = ({ link, name }: { link: string, name: string }): JSX.Element => {
+  return (
+  <li>
+    <NavLink to={`${link}`} className='font-semibold hover:text-rose-500'>{name}</NavLink>
+  </li>
+  )
+}
+
 function NavBar (): JSX.Element {
   const { logout } = useAuth()
   const { toggleTheme } = useTheme()
@@ -11,29 +25,20 @@ function NavBar (): JSX.Element {
   return (
     <nav className='bg-blue-200 py-3 dark:bg-dark-tremor-brand-muted dark:text-white'>
       <ul className='flex items-center justify-around'>
-        <li className='flex'>
-          <img src="/gane.webp" alt="logo de gane" className='w-[70px]' />
-        </li>
-        <div className='flex gap-4'>
-          <li>
-            <NavLink to='/home' className='font-semibold hover:text-rose-600'>Home</NavLink>
-          </li>
-          <li>
-            <NavLink to='/example1' className='font-semibold hover:text-rose-600'>Example 1</NavLink>
-          </li>
-          <li>
-            <NavLink to='/example2' className='font-semibold hover:text-rose-600'>Example 2</NavLink>
-          </li>
-          <li>
-            <NavLink to='/example3' className='font-semibold hover:text-rose-600'>Example 3</NavLink>
-          </li>
+
+        <figure className='flex'>
+          <img src="/gane.webp" alt="logo de gane" className='w-[120px]' />
+        </figure>
+
+        <div className='flex gap-4 text-xl'>
+          {Links.map((link, index) => <LinkComponent key={index} link={link.link} name={link.name} />)}
         </div>
-        <li>
-          <Button onClick={logout}>Cerrar Sesión</Button>
-        </li>
-        <article className='flex rounded-md p-2 h-7 w-16 dark:bg-slate-500'>
+
+        <article className='flex gap-4'>
           <Switch onChange={toggleTheme} />
+          <Button onClick={logout}>Cerrar Sesión</Button>
         </article>
+
       </ul>
     </nav>
   )
