038c0a559e2184c03ae994011c74c3599f90981d
aplicaciones-servired <aplicaciones@gruposervired.com.co>
2024-04-18 11:31:30 -0500

    arreglo

--- client/src/components/iu/cardMetas.tsx
+++ client/src/components/iu/cardMetas.tsx
@@ -5,31 +5,31 @@ export function CardMetas ({ nombre, venta }: { nombre: string, venta: string })
   const [isOpen, setIsOpen] = React.useState(false)
 
   return (
-        <section className="flex justify-center">
-            {[1, 2].map((index) => (
-                <><Card
-                    key={index}
-                    className="-translate-x-50 transition hover:scale-110 max-w-80 mt-6 ml-6 shadow-rose-700 hover:bg-slate-50"
-                    decoration="top"
-                    decorationColor="red"
-                    onClick={() => { setIsOpen(true) }}
-                >
-                    <p className="text-tremor-default text-center mt-1 text-tremor-content dark:text-dark-tremor-content">{nombre}</p>
-                    <p className="text-3xl text-center mt-1 text-tremor-content-strong dark:text-dark-tremor-content-strong font-semibold">{venta}</p>
-                </Card>
-                    <Dialog open={isOpen} onClose={(val) => { setIsOpen(val) }} static={true}>
-                        <DialogPanel>
-                            <CategoryBar
-                                className='mt-1'
-                                values={[40, 30, 20, 10]}
-                                colors={['emerald', 'yellow', 'orange', 'rose']}
-                                markerValue={70} />
-                            <Button className="mt-8 w-full" onClick={() => { setIsOpen(false) }}>
-                                Close
-                            </Button>
-                        </DialogPanel>
-                    </Dialog></>
-            ))}
-        </section>
+    <section className="flex justify-center">
+      {[1, 2].map((index) => (
+        <><Card
+              key={index}
+              className="-translate-x-50 transition hover:scale-110 max-w-80 mt-6 ml-6 shadow-rose-700 hover:bg-slate-50"
+              decoration="top"
+              decorationColor="red"
+              onClick={() => { setIsOpen(true) } }
+          >
+              <p className="text-tremor-default text-center mt-1 text-tremor-content dark:text-dark-tremor-content">{nombre}</p>
+              <p className="text-3xl text-center mt-1 text-tremor-content-strong dark:text-dark-tremor-content-strong font-semibold">{venta}</p>
+          </Card>
+          <Dialog open={isOpen} onClose={(val) => { setIsOpen(val) } } static={true}>
+                  <DialogPanel>
+                      <CategoryBar
+                          className='mt-1'
+                          values={[40, 30, 20, 10]}
+                          colors={['emerald', 'yellow', 'orange', 'rose']}
+                          markerValue={70} />
+                      <Button className="mt-8 w-full" onClick={() => { setIsOpen(false) } }>
+                          Close
+                      </Button>
+                  </DialogPanel>
+              </Dialog></>
+      ))}
+    </section>
   )
 }
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -2,16 +2,30 @@ import { CardUsage } from '../components/iu/card'
 import { CardMetas } from '../components/iu/cardMetas'
 
 function HomePage (): JSX.Element {
-  const nombre = 'META DEL DIA'
-  const venta = '$56,702,929'
+  const data = [
+    {
+      nombre: 'META DEL DIA',
+      venta: '$56,702,929'
+    },
+    {
+      nombre: 'VENTA ACTUAL DEL DIA',
+      venta: '$15,908,201'
+    }
+  ]
 
-  const cumplimiento = '% CUMPLIMIENTO CHANCE DEL DIA YUMBO !!'
-  const porcentaje = '29%'
+  // Utiliza el método map() para renderizar los elementos
+  const renderedCards = data.map((item, index) => (
+    <div key={index}>
+      <CardMetas nombre={item.nombre} venta={item.venta}/>
+    </div>
+  ))
+  const cumplimiento = 'prueba'
+  const porcentaje = '1000'
 
   return (
     <div>
       <CardUsage cumplimiento={cumplimiento} porcentaje={porcentaje}/>
-      <CardMetas nombre={nombre} venta={venta}/>
+      {renderedCards}
     </div>
   )
 }
