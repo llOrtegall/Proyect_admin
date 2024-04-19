1e9c541b7965cc1403f57d297e1da9e4fda62b5f
llOrtegall <IvanOrtega_97@hotmail.com>
2024-04-19 16:48:00 -0500

    validar tables con data actual

--- api/src/connections/mysqlMetas.js
+++ api/src/connections/mysqlMetas.js
@@ -8,6 +8,6 @@ export const pool = createPool({
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  database: process.env.DB_USE,
   connectionLimit: 10
 })
--- api/src/controllers/metas.controllers.js
+++ api/src/controllers/metas.controllers.js
@@ -3,7 +3,17 @@ import { pool } from '../connections/mysqlMetas.js'
 export const getSucursales = async (_req, res) => {
   try {
     const connection = await pool.getConnection()
-    console.log(connection);
+
+    const [rows] = await connection.query("select ASTRO from METASPRODUCTOS where FECHA=CURDATE() and ZONA = '39628'")
+
+    connection.release()
+
+    console.log(rows.length);
+    const sumar = rows.reduce((acc, row) => acc + row.ASTRO, 0)
+
+    console.log(sumar);
+
+    res.status(200).json(rows)
   } catch (error) {
     console.log(error);
   }
