b2c649d525bca17230e9f075392ee1404ac169d0
llOrtegall <IvanOrtega_97@hotmail.com>
2024-04-23 15:02:26 -0500

    crear drop tables

--- api/src/scripts/myTask.js
+++ api/src/scripts/myTask.js
@@ -7,24 +7,31 @@ export const myTask = async () => {
   try {
     const connection = await pool.getConnection()
   
-    const [rows] = await connection.query(`SELECT ${PRODUCTOS} FROM METASPRODUCTOS WHERE FECHA = CURDATE() and ZONA = '39627'`)
+    const [sucursalesMetas] = await connection.query(`SELECT ${PRODUCTOS} FROM METASPRODUCTOS WHERE FECHA = CURDATE() and ZONA = '39627'`)
 
     connection.release();  
 
+    // console.log(sucursalesMetas);
+
     let sucursales = []
     
-    rows.forEach(row => {
+    sucursalesMetas.forEach(row => {
       sucursales.push(row.SUCURSAL)
     })
-    
-    console.log(sucursales);
 
-    // const connect = await pool_test.getConnection()
-
-    // const [rows2] = await connect.query(`INSERT INTO `) 
+    const connection_test = await pool_test.getConnection()
 
 
-    // console.log(' Task Terminate !!! ');
+    for (let i = 0; i < sucursales.length; i++) {
+      const sucursal = sucursales[i]
+      const [result] = await connection_test.query(`
+      INSERT INTO horas_${sucursal} (CHANCE, PAGAMAS, PAGATODO, GANE5, PATA_MILLONARIA, DOBLECHANCE, CHANCE_MILLONARIO, ASTRO, LOTERIA_FISICA, LOTERIA_VIRTUAL, BETPLAY, GIROS, SOAT, RECAUDOS, RECARGAS, PROMO1, PROMO2) VALUES (
+        ${sucursalesMetas[i].CHANCE}, ${sucursalesMetas[i].PAGAMAS}, ${sucursalesMetas[i].PAGATODO}, ${sucursalesMetas[i].GANE5}, ${sucursalesMetas[i].PATA_MILLONARIA}, ${sucursalesMetas[i].DOBLECHANCE}, ${sucursalesMetas[i].CHANCE_MILLONARIO}, ${sucursalesMetas[i].ASTRO}, ${sucursalesMetas[i].LOTERIA_FISICA}, ${sucursalesMetas[i].LOTERIA_VIRTUAL}, ${sucursalesMetas[i].BETPLAY}, ${sucursalesMetas[i].GIROS}, ${sucursalesMetas[i].SOAT}, ${sucursalesMetas[i].RECAUDOS}, ${sucursalesMetas[i].RECARGAS}, ${sucursalesMetas[i].PROMO1}, ${sucursalesMetas[i].PROMO2}
+      )
+      `)
+    }
+
+    console.log('Datos insertados correctamente');
       
   } catch (error) {
     console.log(error);
