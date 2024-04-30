1488817b11d520ad96334942f7d85e3083507953
llOrtegall <IvanOrtega_97@hotmail.com>
2024-04-30 10:21:38 -0500

    remplantenado bakend

--- api/src/routes/horas.routes.js
+++
@@ -1,6 +0,0 @@
-import { getHoras } from '../controllers/horas.controllers.js'
-import { Router } from 'express'
-
-export const horas = Router()
-
-horas.post('/horas', getHoras)
--- api/src/routes/routes.js
+++
@@ -1,6 +0,0 @@
-import { getSucursales } from '../controllers/metas.controllers.js'
-import { Router } from 'express'
-
-export const metas = Router()
-
-metas.get('/sucursales', getSucursales)
