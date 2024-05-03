839942e292797a7c2e441889045a0b0f4676b02b
aplicaciones-servired <aplicaciones@gruposervired.com.co>
2024-05-03 08:44:13 -0500

    metas servired, multired

--- client/src/components/iu/metasDia.tsx
+++ client/src/components/iu/metasDia.tsx
@@ -2,35 +2,40 @@ import axios from 'axios'
 import { useAuth } from '../../auth/AuthProvider'
 import { CardComponent } from './CardComponent'
 import { useEffect, useState } from 'react'
-import { type Metas } from '../../types/metas'
+import { type Metas, type MetasJamundi } from '../../types/metas'
 
 const MetasDia = (): JSX.Element => {
   const { user } = useAuth()
-  const company = user.empresa
-  const [data, setData] = useState<Metas>({})
+  const [data, setData] = useState<Metas | MetasJamundi>({})
   const [percentage, setPercentage] = useState<number>(0)
+
+  const company = user.empresa
   const cumplimiento = `Cumplimiento Del Chance Del Dia ${company}`
+
+  console.log('first', company)
 
   useEffect(() => {
     const fetchMetas = async (): Promise<void> => {
       try {
         const response = await axios.get(`http://localhost:3000/api/metas/${company === 'Servired' ? '39628' : '39627'}`)
-        setData(response.data as Metas)
+        setData(company === 'Servired' ? response.data as MetasJamundi : response.data as Metas)
         console.log('first', response.data)
       } catch (error) {
         console.error('Error fetching data:', error)
       }
     }
 
+    void fetchMetas()
+  }, [company])
+
+  useEffect(() => {
     const calculatePercentage = (): void => {
-      const percentage = Math.round((data.CHANCE + data.PAGAMAS + data.PAGATODO + data.GANE5 + data.PATA_MILLONARIA + data.DOBLECHANCE + data.CHANCE_MILLONARIO) /
-        (data.PROMEDIO_DIARIO_CHANCE + data.PROMEDIO_DIARIO_PAGAMAS + data.PROMEDIO_DIARIO_PAGATODO + data.PROMEDIO_DIARIO_PATAMI + data.PROMEDIO_DIARIO_DOBLECHANCE + data.PROMEDIO_DIARIO_CHMILL) * 100)
+      const percentage = Math.round((data.CHANCE + (data.PAGAMAS || data.CHOLADITO || data.PAGATODO || data.PAGATODO_JAMUNDI || 0) + data.GANE5 + data.PATA_MILLONARIA + data.DOBLECHANCE + data.CHANCE_MILLONARIO) /
+        (data.PROMEDIO_DIARIO_CHANCE + (data.PROMEDIO_DIARIO_PAGAMAS || data.PROMEDIO_DIARIO_CHOLADITO || data.PROMEDIO_DIARIO_PAGATODO || data.PROMEDIO_DIARIO_PGTJAMUNDI || 0) + data.PROMEDIO_DIARIO_PATAMI + data.PROMEDIO_DIARIO_DOBLECHANCE + data.PROMEDIO_DIARIO_CHMILL) * 100)
       setPercentage(percentage)
     }
-
-    void fetchMetas()
     calculatePercentage()
-  }, [company, data])
+  }, [data])
 
   return (
     <>
