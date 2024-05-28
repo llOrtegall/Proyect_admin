76ba6ba5d71574127a69d6cf82846cd9ec278c32
llOrtegall <IvanOrtega_97@hotmail.com>
2024-05-28 12:46:57 -0500

    este codigo se puede mejorar pero almenos algunos calculos ya estan en funtions

--- client/src/components/iu/cardMetas.tsx
+++ client/src/components/iu/cardMetas.tsx
@@ -6,16 +6,32 @@ interface Props {
   porcentaje: number
 }
 
+function DeterminarColor (porcentaje: number): string {
+  if (porcentaje < 20) {
+    return 'red'
+  } else if (porcentaje < 40) {
+    return 'orange'
+  } else if (porcentaje < 60) {
+    return 'amber'
+  } else if (porcentaje < 80) {
+    return 'lime'
+  } else if (porcentaje < 100) {
+    return 'green'
+  } else {
+    return 'emerald'
+  }
+}
+
 // TODO: aqui solo recibe un objecto con dos propiedades, nombre y venta
 export function CardMetas ({ nombre, venta, porcentaje }: Props): JSX.Element {
   console.log(porcentaje)
   return (
-    <Card decoration="top" decorationColor={porcentaje > 80 ? 'emerald' : porcentaje > 50 ? 'lime' : 'red'} >
+    <Card decoration="top" decorationColor={DeterminarColor(porcentaje)} >
       <p className="text-tremor-default text-center mt-1 text-tremor-content-strong dark:text-dark-tremor-content-strong font-semibold">{nombre}</p>
       <p className="text-3xl text-center mt-1 text-tremor-content-strong dark:text-dark-tremor-content-strong font-semibold">
         {nombre === 'GIROS' ? '#' : nombre === 'RECAUDOS' ? '#' : '$'} {new Intl.NumberFormat('en-ES').format(venta)}
       </p>
-      <ProgressCircle className='py-2' size="lg" value={porcentaje} color={porcentaje > 80 ? 'emerald' : porcentaje > 50 ? 'lime' : 'red'}>
+      <ProgressCircle className='py-2' size="lg" value={porcentaje} color={DeterminarColor(porcentaje)}>
         <p className="text-xl text-center mt-1 text-tremor-content-strong dark:text-dark-tremor-content-strong font-semibold">{porcentaje}%</p>
       </ProgressCircle>
     </Card>
