2233f20080f588b6f9f3f21723b0843f0c5aa342
llOrtegall <IvanOrtega_97@hotmail.com>
2024-06-17 11:07:36 -0500

    multired funciona colocar servired

--- client/src/components/DashBoard.tsx
+++ client/src/components/DashBoard.tsx
@@ -7,6 +7,7 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 import { DonutChartComp } from './iu/DonutChart'
+import { Card } from '@tremor/react'
 
 const DahsBoard = ({ company }: { company: Empresa }): JSX.Element => {
   const [data, setData] = useState<Dashboard>({ metaDia: 0, porcentaje: 0, products: [], ventaChance: 0 })
@@ -31,19 +32,22 @@ const DahsBoard = ({ company }: { company: Empresa }): JSX.Element => {
 
   return (
     <>
-      <article className='flex w-full items-center px-2 py-8'>
-        <div className='flex w-8/12 justify-center'>
+      <Card className='flex'>
+        <div className='flex w-8/12 justify-center flex-col items-center gap-2'>
+          <h1 className='text-center text-3xl font-semibold dark:text-white'>Productos Chance {company}</h1>
           <DonutChartComp items={data.products} />
         </div>
 
         {
-          <div className='flex w-3/12 justify-center flex-col gap-10'>
-            <CardDia nombre='Meta Del Día Chance' venta={data?.metaDia} />
-            <CardDia nombre='Venta Actual Día Chance' venta={data?.ventaChance} />
+          <div className='flex w-3/12 justify-center flex-col gap-2'>
+            <h1 className='text-center text-3xl font-semibold dark:text-white'>Cumplimiento {company} </h1>
+            <CardDia nombre='Meta Chance' venta={data?.metaDia} />
+            <CardDia nombre='Venta Actual Chance' venta={data?.ventaChance} />
           </div>
         }
-      </article>
+      </Card>
 
+      <h1 className='text-center text-2xl py-2 font-semibold dark:text-white'>Resumen Cumplimiento Productos y/o Servicios {company}</h1>
       <section className='grid grid-cols-4 gap-2 px-2'>
         {
           data.products.map(item => (
